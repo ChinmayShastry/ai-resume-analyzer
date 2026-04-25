be649477e1c7b76bfde1b99aaf76404513cef594
Chinmay Shastry <200456357+ChinmayShastry@users.noreply.github.com>
2026-04-25 10:12:01 +0530

    Update scorer.py

--- services/scorer.py
+++ services/scorer.py
@@ -1,3 +1,10 @@
-def calculate_score(similarity):
-    # simple version (improve later)
-    return round(similarity * 100, 2)
+def calculate_score(similarity, gap, sections_quality=0.5):
+    skill_score = gap["match_percentage"] / 100
+
+    final_score = (
+        0.4 * skill_score +
+        0.4 * similarity +
+        0.2 * sections_quality
+    )
+
+    return round(final_score * 100, 2)
